16a4600d5449e8c0e3654270417fc5b5fc6b0fb0
Moazzam Farooqui <moazzam12345678@gmail.com>
2025-10-02 18:36:08 +0500

    Add files via upload

---
+++ LAB#3 Q3.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+   public:
+     int data;
+	 Node*next;
+	 Node*prev;
+	 
+	Node(int val){
+		data=val;
+		next=nullptr;
+		prev=nullptr;
+	}	
+};
+
+class DoublyLinkedList{
+	private:
+		Node*head;
+		Node*tail;
+		
+	public:
+		DoublyLinkedList(){
+			head=nullptr;
+			tail=nullptr;
+		}
+		
+		void insertrear(int val){
+			Node*newnode=new Node(val);
+			if(head==nullptr){
+				head=newnode;
+				tail=newnode;
+				return;
+			}
+			tail->next=newnode;
+			newnode->prev=tail;
+			tail=newnode;
+		}
+		
+		void concatenate(DoublyLinkedList&other){
+			if(other.head==nullptr)return;
+			if(this->head==nullptr){
+				head=other.head;
+				tail=other.tail;
+				return;
+			}
+			tail->next=other.head;
+			other.head->prev=tail;
+			tail=other.tail;
+		}
+		
+		void display(){
+			Node*temp=head;
+			while(temp!=nullptr){
+				cout<<temp->data<<" ";
+				temp=temp->next;
+			}
+			cout<<endl;
+		}
+};
+
+int main(void){
+	DoublyLinkedList L;
+	DoublyLinkedList M;
+	DoublyLinkedList N;
+	
+	for(int i=2;i<=10;i+=2){
+		L.insertrear(i);
+	}
+	
+	for(int i=1;i<=9;i+=2){
+		M.insertrear(i);
+	}
+	
+	cout<<"LIST L(EVEN):"<<endl;
+	L.display();
+	
+	cout<<"LIST M(ODD):"<<endl;
+	M.display();
+	
+	N.concatenate(L);
+	N.concatenate(M);
+	cout<<"LIST N:"<<endl;
+	N.display();
+	return 0;
+}
