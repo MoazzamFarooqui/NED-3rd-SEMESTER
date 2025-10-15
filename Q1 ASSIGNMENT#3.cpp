f840d041be1092da4d2fc711bb08693341e4d2fb
Moazzam Farooqui <moazzam12345678@gmail.com>
2025-10-15 23:30:01 +0500

    Add files via upload

---
+++ Q1 ASSIGNMENT#3.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int factorial(int n){
+	if(n==0 || n==1){
+		return 1;
+	}
+    else{
+    	return n*factorial(n-1);
+	}
+}
+
+int main(void){
+    int n;
+    cout<<"ENTER A NUMBER:";
+    cin>>n;
+    cout<<"FACTORIAL OF "<<n<<" IS "<<factorial(n);
+    return 0;
+}
+
+
+
+
